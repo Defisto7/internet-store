dc3cfbd0a5b9de5f526561c3a0feea6ea7d2409a
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-06-17 12:05:22 +0300

    Master
    Adding Individual Meal Items

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,8 @@
 import React from 'react'
+import Card from '../UI/Card';
 import s from './AvailableMeals.module.css'
+import MealItem from './MealItem/MealItem';
+
 
 const DUMMY_MEALS = [
   {
@@ -29,15 +32,21 @@ const DUMMY_MEALS = [
 ];
 
 function AvailableMeals() {
-  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem 
+      key={meal.id} 
+      name={meal.name} 
+      description={meal.description} 
+      price={meal.price} />
+  ));
 
   return (
     <section className={s.meals}>
-      <ul>
-        {mealsList}
-      </ul>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
     </section>
-  )
+  );
 }
 
 export default AvailableMeals
