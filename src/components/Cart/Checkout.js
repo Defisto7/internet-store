869b92c4b038ee6c830efec214a89c00d60c03b4
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-06-27 17:49:45 +0300

    Master
    Reading Form Values

--- src/components/Cart/Checkout.js
+++ src/components/Cart/Checkout.js
@@ -1,29 +1,47 @@
+import { useRef } from 'react';
+
 import s from './Checkout.module.css'
 
 function Checkout(props) {
+  const nameInputRef = useRef();
+  const streetInputRef = useRef();
+  const postalCodeInputRef = useRef();
+  const cityInputRef = useRef();
+
   const canfirmHandler = (e) => {
     e.preventDefault();
+
+    const enteredName = nameInputRef.current.value;
+    const streetInput = streetInputRef.current.value;
+    const postalCodeInput = postalCodeInputRef.current.value;
+    const cityInput = cityInputRef.current.value;
   }
-  return <form onSubmit={canfirmHandler}>
-    <div className={s.control}>
-      <label htmlFor='name'>Your name</label>
-      <input type='text' id='name'/>
-    </div>
-    <div className={s.control}>
-      <label htmlFor='street'>Street</label>
-      <input type='text' id='street'/>
-    </div>
-    <div className={s.control}>
-      <label htmlFor='postal'>Postal Code</label>
-      <input type='text' id='postal'/>
-    </div>
-    <div className={s.control}>
-      <label htmlFor='city'>City</label>
-      <input type='text' id='city'/>
-    </div>
-    <button type="button" onClick={props.onCancel}>Cancel</button>
-    <button>Confirm</button>
-  </form>
+  return (
+    <form onSubmit={canfirmHandler}>
+      <div className={s.control}>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" ref={nameInputRef}/>
+      </div>
+      <div className={s.control}>
+        <label htmlFor="street">Street</label>
+        <input type="text" id="street" ref={streetInputRef}/>
+      </div>
+      <div className={s.control}>
+        <label htmlFor="postal">Postal Code</label>
+        <input type="text" id="postal" ref={postalCodeInputRef}/>
+      </div>
+      <div className={s.control}>
+        <label htmlFor="city">City</label>
+        <input type="text" id="city" ref={cityInputRef}/>
+      </div>
+      <div className={s.actions}>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
+        <button className={s.submit}>Confirm</button>
+      </div>
+    </form>
+  );
 }
 
 export default Checkout
