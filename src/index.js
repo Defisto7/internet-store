190f78e6b6e30cbf2f7ae91bf89927231332090f
Dima Yanovskji <86802309+Defisto7@users.noreply.github.com>
2022-07-07 16:18:45 +0300

    Master
    fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom"
 
 import './index.css';
 import App from './App';
 
-createRoot(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
